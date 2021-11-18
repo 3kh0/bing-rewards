05801a7bcb2d99b6be5ee85ea504f8143434b14f
Justin Wong <justin.wong235@gmail.com>
2021-11-17 19:45:06 -0500

    cookie error handling

--- BingRewards/src/driver.py
+++ BingRewards/src/driver.py
@@ -6,7 +6,7 @@ import zipfile
 from selenium import webdriver
 from selenium.webdriver.support.abstract_event_listener import AbstractEventListener
 from selenium.webdriver.support.event_firing_webdriver import EventFiringWebDriver
-from selenium.common.exceptions import SessionNotCreatedException
+from selenium.common.exceptions import SessionNotCreatedException, WebDriverException
 import re
 
 
@@ -29,7 +29,7 @@ class Driver:
     __WEB_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36 Edg/94.0.992.50"
     __MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 10; HD1913) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.50 Mobile Safari/537.36 EdgA/93.0.961.53"
 
-    def __download_driver(driver_path, system, try_count=0):
+    def __download_driver(driver_path, system, dl_try_count=0):
         # determine latest chromedriver version
         #version selection faq: http://chromedriver.chromium.org/downloads/version-selection
         CHROME_RELEASE_URL = "https://sites.google.com/chromium.org/driver/downloads?authuser=0"
@@ -46,7 +46,7 @@ class Driver:
 
         latest_version = re.findall(
             b"ChromeDriver \d{2,3}\.0\.\d{4}\.\d+", response
-        )[try_count].decode().split()[1]
+        )[dl_try_count].decode().split()[1]
         print('Downloading chromedriver version: ' + latest_version)
 
         if system == "Windows":
@@ -93,6 +93,40 @@ class Driver:
         os.rmdir(extracted_dir)
         os.chmod(driver_path, 0o755)
 
+    def add_chrome_options(device, headless, cookies):
+        options = webdriver.ChromeOptions()
+
+        options.add_argument("--disable-extensions")
+        options.add_argument("--window-size=1280,1024")
+        options.add_argument("--log-level=3")
+        options.add_argument("--disable-notifications")
+        options.add_argument("disable-infobars")
+
+        options.add_experimental_option(
+            "prefs", {
+                # geolocation permission, 0=Ask, 1=Allow, 2=Deny
+                "profile.default_content_setting_values.geolocation": 1,
+                "profile.default_content_setting_values.notifications": 2
+            }
+        )
+
+        if headless:
+            options.add_argument("--headless")
+
+        if device == Driver.WEB_DEVICE:
+            options.add_argument("user-agent=" + Driver.__WEB_USER_AGENT)
+        else:
+            options.add_argument("user-agent=" + Driver.__MOBILE_USER_AGENT)
+
+        if cookies:
+            options.add_argument("user-data-dir=stored_browser_data")
+            #option 1
+            options.add_argument('--remote-debugging-port=9222')
+            #option 2
+            #options.add_argument('--no-sandbox')
+            #options.add_argument('--disable-dev-shm-usage')
+        return options
+
     def get_driver(path, device, headless, cookies):
         system = platform.system()
         if system == "Windows":
@@ -101,45 +135,32 @@ class Driver:
         if not os.path.exists(path):
             Driver.__download_driver(path, system)
 
-        options = webdriver.ChromeOptions()
-        options.add_argument("--disable-extensions")
-        options.add_argument("--window-size=1280,1024")
-        options.add_argument("--log-level=3")
-        options.add_argument("--disable-notifications")
-        options.add_argument("disable-infobars")
-
-        if cookies:
-            options.add_argument("user-data-dir=stored_browser_data")
-        options.add_experimental_option(
-            "prefs", {
-                "profile.default_content_setting_values.geolocation": 1,
-                "profile.default_content_setting_values.notifications": 2
-            }
-        )  # geolocation permission, 0=Ask, 1=Allow, 2=Deny
-        if headless:
-            options.add_argument("--headless")
-        #else:
-        #    options.add_argument("--window-position=-2000,0") # doesnt move off screen
-
-        if device == Driver.WEB_DEVICE:
-            options.add_argument("user-agent=" + Driver.__WEB_USER_AGENT)
-        else:
-            options.add_argument("user-agent=" + Driver.__MOBILE_USER_AGENT)
-
-        # we start at try_count = 1 b/c we already downloaded the most recent version
-        try_count = 1
+        # we start at dl_try_count = 1 b/c we already downloaded the most recent version
+        dl_try_count = 1
+        cookie_try_count = 0
         MAX_TRIES = 3
         is_dl_success = False
+        options = Driver.add_chrome_options(device, headless, cookies)
+
         while not is_dl_success:
             try:
                 driver = webdriver.Chrome(path, options=options)
                 is_dl_success = True
-            #driver not up to date with Chrome browser, try different ver
+
+            #Cookies option causing issues
+            except WebDriverException:
+                if cookie_try_count == 1:
+                    raise WebDriverException('Driver cookie option issue?')
+                print('Trying chromedriver without cookies option')
+                options = Driver.add_chrome_options(device, headless, cookies=False)
+                cookie_try_count += 1
+
+            #driver not up to date with Chrome browser, try different version
             except SessionNotCreatedException:
-                if try_count == MAX_TRIES:
-                    raise SessionNotCreatedException(f'Tried downloading the {try_count} most recent chromedrivers. None match your Chrome browswer version. Aborting now, please update your chrome browser.')
-                Driver.__download_driver(path, system, try_count)
-                try_count += 1
+                if dl_try_count == MAX_TRIES:
+                    raise SessionNotCreatedException(f'Tried downloading the {dl_try_count} most recent chromedrivers. None match your Chrome browswer version. Aborting now, please update your chrome browser.')
+                Driver.__download_driver(path, system, dl_try_count)
+                dl_try_count += 1
 
         #if not headless:
         #    driver.set_window_position(-2000, 0)
