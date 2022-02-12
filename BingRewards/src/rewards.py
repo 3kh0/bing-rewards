df98df0148b65b6f2ab47351e79f054764652630
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2022-02-12 12:31:40 -0500

    fix locale for Mac as well

--- BingRewards/src/rewards.py
+++ BingRewards/src/rewards.py
@@ -249,7 +249,7 @@ class Rewards:
                 )
             )  # sleep at least 20 seconds to avoid over requesting server
 
-        if self._ON_POSIX: #TODO: fix locale for windows
+        if self._ON_POSIX and locale.getlocale()[0] is not None:
             (lang, geo) = locale.getlocale()[0].split("_")  # en and US
         else:
             lang = "en"
