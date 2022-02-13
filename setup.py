b8eae91666dc312e64934ada715e77e6b02218e3
Justin Wong <justin.wong235@gmail.com>
2022-02-13 13:39:18 -0500

    in setup.py, allow the user to skip inputs without losing previous values

--- setup.py
+++ setup.py
@@ -3,8 +3,30 @@ import getpass
 import base64
 import time
 
-CONFIG_FILE_PATH = "BingRewards/src/config.py"
 
+def encode_input(s):
+    return base64.b64encode(s.encode()).decode()
+
+
+def use_old_or_new(prev_val, new_val):
+    '''
+    Use the new inputted value if the user didn't skip input
+    Else just use the old one
+    '''
+    if new_val:
+        return new_val
+    return prev_val
+
+
+def update_value(input_value, attribute_type):
+    old_value = credentials.get(attribute_type)
+    new_value = encode_input(input_value)
+    final_value = use_old_or_new(old_value, new_value)
+    print(f"   Hashed: {final_value}\n")
+    return final_value
+
+
+CONFIG_FILE_PATH = "BingRewards/src/config.py"
 CONFIG_FILE_TEMPLATE = """credentials = dict(
     email = '{0}',
     password = '{1}',
@@ -13,21 +35,18 @@ CONFIG_FILE_TEMPLATE = """credentials = dict(
 )
 """
 
+if os.path.isfile(CONFIG_FILE_PATH):
+    from BingRewards.src.config import credentials
+else:
+    credentials = {}
 
-def encode_input(s):
-    return base64.b64encode(s.encode()).decode()
+print('Note: To SKIP a field, press <ENTER>. If skipped, will use current value if it exists.\n')
 
+email = update_value(input("*MS Rewards Email: "), 'email')
+password = update_value(getpass.getpass("*Password: "), 'password')
 
-# get hashed credentials
-email = encode_input(input("   *Email: "))
-print(f"   Hashed: {email}\n")
-password = encode_input(getpass.getpass("*Password: "))
-print(f"   Hashed: {password}\n")
-
-telegram_api_token = encode_input(input("*Telegram API Token (optional, press enter to skip): "))
-print(f"   Hashed: {telegram_api_token}\n")
-telegram_userid = encode_input(input("*Telegram User ID (optional, press enter to skip): "))
-print(f"   Hashed: {telegram_userid}\n")
+telegram_api_token = update_value(input("*Telegram API Token (optional)"), 'telegram_api_token')
+telegram_userid = update_value(input("*Telegram User ID (optional)"), 'telegram_userid')
 
 new_config = CONFIG_FILE_TEMPLATE.format(email, password, telegram_api_token, telegram_userid)
 
