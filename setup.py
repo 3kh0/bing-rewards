cee7bb4e0eddbed61389c76d19946a53a1fa1bf5
Justin Wong <justin.wong235@gmail.com>
2022-02-16 12:25:18 -0500

    fix setup.py None bug

--- setup.py
+++ setup.py
@@ -8,22 +8,24 @@ def encode_input(s):
     return base64.b64encode(s.encode()).decode()
 
 
-def use_old_or_new(prev_val, new_val):
+def get_final_encoded(prev_encoded, new_encoded):
     '''
     Use the new inputted value if the user didn't skip input
     Else just use the old one
     '''
-    if new_val:
-        return new_val
-    return prev_val
+    if new_encoded:
+        return new_encoded
+    return prev_encoded
 
 
 def update_value(input_value, attribute_type):
-    old_value = credentials.get(attribute_type)
-    new_value = encode_input(input_value)
-    final_value = use_old_or_new(old_value, new_value)
-    print(f"   Hashed: {final_value}\n")
-    return final_value
+    if input_value == 'C':
+        return ''
+    old_encoded = credentials.get(attribute_type, '')
+    new_encoded = encode_input(input_value)
+    final_encoded = get_final_encoded(old_encoded, new_encoded)
+    print(f"   Hashed: {final_encoded}\n")
+    return final_encoded
 
 
 CONFIG_FILE_PATH = "BingRewards/src/config.py"
@@ -40,13 +42,14 @@ if os.path.isfile(CONFIG_FILE_PATH):
 else:
     credentials = {}
 
-print('Note: To SKIP a field, press <ENTER>. If skipped, will use current value if it exists.\n')
+print('\nInstructions:\nTo SKIP or to KEEP current value, press <ENTER>.\nTo CLEAR value altogether, enter "C".\n')
+time.sleep(2)
 
 email = update_value(input("*MS Rewards Email: "), 'email')
 password = update_value(getpass.getpass("*Password: "), 'password')
 
-telegram_api_token = update_value(input("*Telegram API Token (optional)"), 'telegram_api_token')
-telegram_userid = update_value(input("*Telegram User ID (optional)"), 'telegram_userid')
+telegram_api_token = update_value(input("*Telegram API Token (optional): "), 'telegram_api_token')
+telegram_userid = update_value(input("*Telegram User ID (optional): "), 'telegram_userid')
 
 new_config = CONFIG_FILE_TEMPLATE.format(email, password, telegram_api_token, telegram_userid)
 
