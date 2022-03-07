8777a32aa4568d5be4e6e5898166adcb9d650052
Justin Wong <justin.wong235@gmail.com>
2022-03-07 18:14:56 -0500

    modify telegram error msg

--- BingRewards/src/telegram.py
+++ BingRewards/src/telegram.py
@@ -9,15 +9,15 @@ class TelegramMessenger():
 
     def send_message(self, message):
         reply_url = f'https://api.telegram.org/bot{self.api_token}/sendMessage?chat_id={self.userid}&text={message}'
-        return requests.get(reply_url)
+        resp = requests.get(reply_url)
+        if resp.status_code == 200:
+            print("Telegram notification sent\n")
+        else:
+            print(f"Boo! Telegram notification NOT sent, response code is: {resp} with response msg {resp.text}\n")
 
     def send_reward_message(self, stats, email):
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         message = f'\n Summary for {email} at : {current_time} \n\n' + \
         "\n".join(stats.stats_str)
 
-        resp = self.send_message(message)
-        if resp.status_code == 200:
-            print("Telegram notification sent\n")
-        else:
-            print(f"Boo! Telegram notification NOT sent, response is: {resp}\n")
+        self.send_message(message)
