0da8290011c0c3d21dbd13acc203bd371a6b1f22
Justin Wong <63d6f389614cb4ba53fda7e3@bots.bitbucket.org>
2023-02-12 14:27:12 -0500

    use .dockerignore to exclude /drivers

--- entry.sh
+++ entry.sh
@@ -13,16 +13,6 @@ echo "Setting Up pip and installing requirements"
 pip install --upgrade pip
 pip install --no-warn-script-location -r /bing-rewards/BingRewards/requirements.txt
 
-# Remove /drivers dir if exists as it was copied over from local machine which could be different OS
-export dir_name='/bing-rewards/BingRewards/drivers'
-echo "If $dir_name directory exists, remove from container"
-if [ -d "$dir_name" ]; then
-  echo "Directory $dir_name exists, removing it and all its contents..."
-  rm -rf "$dir_name"
-else
-  echo "Directory $dir_name does not exist, nothing to remove."
-fi
-
 echo "Checking for imported logs"
 if test -e "$log";
 then
